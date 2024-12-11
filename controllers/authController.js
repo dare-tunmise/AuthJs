5aff3b43d730bc3159a0889f14b880ebc1459d3d
dare-tunmise <daretunmise@yahoo.com>
2024-12-11 18:45:02 +0100

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,17 @@
+const {signupSchema} = require('../middlewares/validator');
+
 exports.signup = async (req, res)=> {
     const { email, password } = req.body;
     try {
+        const {error, value} = signupSchema.validate({email, password});
+        if(error){
+            return res.stattus(401).json({success:false, message: error.details[0].message})
+        }
+        const existingUser = await UserActivation.findOne({email});
 
+        if(existingUser) {
+            return res.stattus(401)
+        }
     } catch(error) {
         console(error);
     }
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,5 +1,8 @@
 const Joi = require('joi');
 
 exports.signupSchema = Joi.object({
-    email: Joi.string().min(6).max(60).required.email
+    email: Joi.string().min(6).max(60).required.email({
+        tlds: { allow:['com', 'net']}
+    }),
+    paswword: Joi.string().required().pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'))
 })
